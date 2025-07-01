37a6a5202bdfbe688b6f64cfbeb6eb9195550dcf
ibrohim <ibrohim.dev.uz@gmail.com>
2025-07-01 22:58:17 +0500

    .

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Categories, CourseCategories, Course,
+    CourseSections, CourseReviews, Lessons
+)
+
+
+@admin.register(Categories)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'id')
+    search_fields = ('name', )
+
+
+@admin.register(CourseCategories)
+class CourseCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category_id', 'id')
+    search_fields = ('name', )
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'short_description', 'price', 'discount_price', 'is_published', 'created_at', 'updated_at', 'id')
+    search_fields = ('title', 'price')
+
+
+@admin.register(CourseSections)
+class CourseSectionsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course_id', 'id')
+    search_fields = ('title', )
+
+
+@admin.register(Lessons)
+class LessonsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'section_id', 'video_url', 'duration', 'is_preview', 'id')
+    search_fields = ('title', )
+
+
+@admin.register(CourseReviews)
+class CourseReviewsAdmin(admin.ModelAdmin):
+    list_display = ('course_id', 'user_id', 'rating', 'created_at', 'id')
+    search_fields = ('rating', )
--- course/models.py
+++ course/models.py
@@ -1,24 +1,39 @@
+import uuid
 from django.db.models import (Model, CharField, TextField, TimeField,ForeignKey, CASCADE, DecimalField,
-    BooleanField, IntegerField)
+    BooleanField, IntegerField, SlugField)
 from django.utils.timezone import now
+from django.utils.text import slugify
+
 
 
 class Categories(Model):
     name = CharField(max_length=255)
-    slug = CharField(max_length=255)
+    slug = SlugField(unique=True, max_length=255, editable=False)
     description = TextField()
     created_at = TimeField(default=now)
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        if Categories.objects.filter(slug=self.slug).exists():
+            self.slug += uuid.uuid4().__str__().split('-')[-1]
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.name
 
 class CourseCategories(Model):
     name = CharField(max_length=255)
     category_id = ForeignKey(Categories, CASCADE, related_name="course_category")
 
+    def __str__(self):
+        return self.name
+
 
 class Course(Model):
     title = CharField(max_length=255)
     course_category_id = ForeignKey(CourseCategories, CASCADE, related_name="course")
-    slug = CharField(max_length=255)
+    slug = SlugField(unique=True, max_length=255, editable=False)
     description = TextField()
     short_description = CharField(max_length=255)
     price = DecimalField(decimal_places=2, max_digits=10)
@@ -28,6 +43,16 @@ class Course(Model):
     created_by = TimeField()
     created_at = TimeField(default=now)
     updated_at = TimeField(default=now)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        if Course.objects.filter(slug=self.slug).exists():
+            self.slug += uuid.uuid4().__str__().split('-')[-1]
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
 
 
 class CourseSections(Model):
