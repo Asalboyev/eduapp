0b25bed92c543815bfbedcc6d97afc5e7d58f666
codem-py <ibehruz005@gmail.com>
2025-07-05 16:08:56 +0500

    category update delete , settings

--- course/urls.py
+++ course/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CategoryListView, CategoryCreateView
+from .views import CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView
 
 urlpatterns = [
     path('category-list/', CategoryListView.as_view(), name='category-list'),
     path('category/create/', CategoryCreateView.as_view(), name='category-create'),
+    path('category/update/<int:pk>/', CategoryUpdateView.as_view(), name='category-update'),
+    path('category/delete/<int:pk>/', CategoryDeleteView.as_view(), name='category-delete'),
 ]
